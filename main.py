31569f548dea35e9c0a4c2ced9a6ace7dffeb2ca
rpsmith77 <44273059+rpsmith77@users.noreply.github.com>
2021-10-07 18:34:48 -0400

    Just playing around forking and pull requests

--- main.py
+++ main.py
@@ -1,21 +1,16 @@
 
 
 def add(num_One, num_Two):
-    #TODO: Write a function to add two numbers together and return the value
     return num_One + num_Two
-    pass
 
 def subtract(num_One, num_Two):
-    #TODO: Write a function to subtract num_Two from num_One
-    pass
+    return num_One - num_Two
 
 def multiply(num_One, num_Two):
     return num_One * num_Two
-    pass
 
 def divide(dividend, divisor):
-    #TODO: Write a function to divides two numbers together and return the value
-    pass
+    return dividend / divisor
 
 def sum(nums: list):
     total = 0
@@ -23,10 +18,7 @@ def sum(nums: list):
         total += number
     return total
 
-
-
-if __name__ == '__main__':
-
+def main():
     value_one = 121
     value_two = 11
 
@@ -34,3 +26,6 @@ if __name__ == '__main__':
     print("The subtraction of", value_one, "by", value_two, "is:", subtract(value_one, value_two))
     print("The multiplication of", value_one, "and", value_two, "is:", multiply(value_one, value_two))
     print("The division of", value_one, "by", value_two, "is:", divide(value_one, value_two))
+
+if __name__ == '__main__':
+    main()
