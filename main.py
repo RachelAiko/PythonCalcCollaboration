7fa2cd07dae22fc248ad4ee2f6881435f9cc71a0
Jairo Garciga <jgarciga0822@eagle.fgcu.edu>
2021-10-07 15:51:41 -0400

    division!

--- main.py
+++ main.py
@@ -1,32 +1,35 @@
-
-
 def add(num_One, num_Two):
-    #TODO: Write a function to add two numbers together and return the value
+    # TODO: Write a function to add two numbers together and return the value
     pass
+
 
 def subtract(num_One, num_Two):
-    #TODO: Write a function to subtract num_Two from num_One
+    # TODO: Write a function to subtract num_Two from num_One
     pass
+
 
 def multiply(num_One, num_Two):
-    #TODO: Write a function to multiply two numbers together and return the value
+    # TODO: Write a function to multiply two numbers together and return the value
     pass
+
 
 def divide(dividend, divisor):
-    #TODO: Write a function to divides two numbers together and return the value
+    return dividend // divisor
     pass
 
+
 def sum(nums: list):
-    #TODO: Write a function that sums up the values in a list and return the resulting value
+    # TODO: Write a function that sums up the values in a list and return the resulting value
     pass
 
 
 if __name__ == '__main__':
-
     value_one = 121
     value_two = 11
+    values = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20]
 
     print("The sum of", value_one, "and", value_two, "is:", add(value_one, value_two))
     print("The subtraction of", value_one, "by", value_two, "is:", subtract(value_one, value_two))
     print("The multiplication of", value_one, "and", value_two, "is:", multiply(value_one, value_two))
     print("The division of", value_one, "by", value_two, "is:", divide(value_one, value_two))
+    print("The division of the list", values, "is:", sum(values))
