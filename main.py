5a3d443bee34fbcad0dc26e6c9fa587caad6d008
Jgar157 <54415437+Jgar157@users.noreply.github.com>
2021-10-07 15:41:57 -0400

    Create main.py

---
+++ main.py
@@ -0,0 +1,32 @@
+
+
+def add(num_One, num_Two):
+    #TODO: Write a function to add two numbers together and return the value
+    pass
+
+def subtract(num_One, num_Two):
+    #TODO: Write a function to subtract num_Two from num_One
+    pass
+
+def multiply(num_One, num_Two):
+    #TODO: Write a function to multiply two numbers together and return the value
+    pass
+
+def divide(dividend, divisor):
+    #TODO: Write a function to divides two numbers together and return the value
+    pass
+
+def sum(nums: list):
+    #TODO: Write a function that sums up the values in a list and return the resulting value
+    pass
+
+
+if __name__ == '__main__':
+
+    value_one = 121
+    value_two = 11
+
+    print("The sum of", value_one, "and", value_two, "is:", add(value_one, value_two))
+    print("The subtraction of", value_one, "by", value_two, "is:", subtract(value_one, value_two))
+    print("The multiplication of", value_one, "and", value_two, "is:", multiply(value_one, value_two))
+    print("The division of", value_one, "by", value_two, "is:", divide(value_one, value_two))
