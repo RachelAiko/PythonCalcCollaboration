13f84f50102b2a8d781d7b9c9b624a3193f94621
Ryan Smith <rpsmith77@gmail.com>
2021-10-08 15:31:28 -0400

    added docstring

--- main.py
+++ main.py
@@ -1,22 +1,30 @@
+"""
+Demo colab w/ github
+"""
 
 
 def add(num_One, num_Two):
     return num_One + num_Two
 
+
 def subtract(num_One, num_Two):
     return num_One - num_Two
+
 
 def multiply(num_One, num_Two):
     return num_One * num_Two
 
+
 def divide(dividend, divisor):
     return dividend / divisor
+
 
 def sum(nums: list):
     total = 0
     for number in nums:
         total += number
     return total
+
 
 def main():
     value_one = 121
@@ -26,6 +34,7 @@ def main():
     print("The subtraction of", value_one, "by", value_two, "is:", subtract(value_one, value_two))
     print("The multiplication of", value_one, "and", value_two, "is:", multiply(value_one, value_two))
     print("The division of", value_one, "by", value_two, "is:", divide(value_one, value_two))
+
 
 if __name__ == '__main__':
     main()
